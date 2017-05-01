57c7b7de4efadbb478cbcd2d8f9cf6966fcfe5a4
Ilya Gusarov <indoos@mail.ru>
2017-05-01 14:40:22 +0400

    refactored folder structure

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import './Photo.css';
-import * as actions from './actions';
+import * as actions from '../actions';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import classNames from 'classnames';
--- src/index.js
+++ src/index.js
@@ -1,66 +1,67 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './components/App';
 import './index.css';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
 import reducer from './reducers';
 
+const imgPath = 'images/';
 const initialState = {
   photos: [
     {
       id: 1,
       title: 'sun glasses',
-      src: '1.jpg',
+      src: imgPath + '1.jpg',
       score: 0,
       selected: true
     },
     {
       id: 2,
       title: 'kitten',
-      src: '2.jpg',
+      src: imgPath + '2.jpg',
       score: 0,
       selected: true
     },
     {
       id: 3,
       title: 'squirrel',
-      src: '3.jpg',
+      src: imgPath + '3.jpg',
       score: 0,
       selected: true
     },
     {
       id: 4,
       title: 'kittens playing',
-      src: '4.jpg',
+      src: imgPath + '4.jpg',
       score: 0,
       selected: true
     },
     {
       id: 5,
       title: 'owl',
-      src: '5.jpg',
+      src: imgPath + '5.jpg',
       score: 0,
       selected: true
     },
     {
       id: 6,
       title: 'monkeys',
-      src: '6.jpg',
+      src: imgPath + '6.jpg',
       score: 0,
       selected: true
     },
     {
       id: 7,
       title: 'buterfly',
-      src: '7.jpg',
+      src: imgPath + '7.jpg',
       score: 0,
       selected: true
     },
     {
       id: 8,
       title: 'bird',
-      src: '8.jpg',
+      src: imgPath + '8.jpg',
       score: 0,
       selected: true
     }
